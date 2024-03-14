220aad0a41a68a9c8a68105c85585452f83de000
cristian gonzalez <crisgonzalez26@gmail.com>
2024-03-14 03:39:01 -0300

    Agregar Validaciones al Formulario de Registro de Usuarios

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom';
 import AuthUser from './AuthUser';
+import { useForm } from "react-hook-form";
+import swal from 'sweetalert';
 
 function Register() {
     const navigate = useNavigate();
@@ -8,48 +10,79 @@ function Register() {
     const [name,setName] = useState();
     const [email,setEmail] = useState();
     const [password,setPassword] = useState();
+    const [errorRegistro,setErrorRegistro] = useState();
+    const { register, handleSubmit,  formState: { errors } } = useForm({
+        shouldUseNativeValidation: true,
+      });
 
-    const submitForm = () =>{
-        // api call
-        http.post('/register',{name:name,email:email,password:password}).then((res)=>{
-            navigate('/login')
+    const onSubmit = () => {
+        //console.log(errors);
+        setErrorRegistro(false);
+        http.post('/register',{name:name,email:email,password:password})
+        .then((res)=>{
+            console.log('register',res);
+            if(res.request.status === 200)
+            {
+                swal("Éxito!",res.data.message,"success");
+                navigate('/login')
+            }
+            else if(res.request.status === 401)
+            {
+                swal("Error",res.request.message,"error");
+            }
         })
+        .catch((e) => {
+            console.log('entro',errors);
+            setErrorRegistro(true);
+        });
     }
-
     const messages = {
         req: "Este campo es obligatorio",
         name: "El formato introducido no es el correcto",
         mail: "Debes introducir una dirección correcta"
        };
-       
-    return(
-        <div className="row justify-content-center pt-5">
-            <div className="col-sm-6">
-                <div className="card p-4">
-                    <h1 className="text-center mb-3">Registro </h1>
-                    <div className="form-group">
-                        <label>Nombre:</label>
-                        <input type="text" className="form-control" placeholder="Imgresar Nombre"
-                            onChange={e=>setName(e.target.value)}
-                        id="name" />
-                    </div>
-                    <div className="form-group mt-3">
-                        <label>E-mail:</label>
-                        <input type="email" className="form-control" placeholder="Ingresar e-mail"
-                            onChange={e=>setEmail(e.target.value)}
-                        id="email" />
-                    </div>
 
-                    <div className="form-group mt-3">
-                        <label>Password:</label>
-                        <input type="password" className="form-control" placeholder="Ingresar password"
-                            onChange={e => setPassword(e.target.value)}
-                        id="password" />
+    const patterns = {
+        mail:/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    };
+
+    return(
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="row justify-content-center pt-5">
+                <div className="col-sm-6">
+                    <div className="card p-4">
+                        <h1 className="text-center mb-3">Registro </h1>
+                        <div className="form-group">
+                            <label>Nombre:</label>
+                            <input type="text" className="form-control" placeholder="Imgresar Nombre"
+                                onChange={e=>setName(e.target.value)}
+                            id="name" {...register("name", { required: messages.req })}/>
+                        </div>
+                        {errors.name && <p className='text-danger'>{errors.name.message}</p>}
+                        <div className="form-group mt-3">
+                            <label>E-mail:</label>
+                            <input type="email" className="form-control" placeholder="Ingresar e-mail"
+                                onChange={e=>setEmail(e.target.value)}
+                                id="email" {...register("email", { required: messages.req,
+                                pattern: {
+                                    value: patterns.mail,
+                                    message: messages.mail
+                                } })}/>
+                        </div>
+                        {errors.email && <p className='text-danger'>{errors.email.message}</p>}
+                        <div className="form-group mt-3">
+                            <label>Password:</label>
+                            <input type="password" className="form-control" placeholder="Ingresar password"
+                                onChange={e => setPassword(e.target.value)}
+                            id="password" {...register("password", { required: messages.req })}/>
+                        </div>
+                        {errors.password && <p className='text-danger'>{errors.password.message}</p>}
+                        {errorRegistro && <p className='text-danger'>Hubo un problema a registrar el Usuario.</p>}
+                        <button type="submit" className="btn btn-primary mt-4">Registrar</button>
                     </div>
-                    <button type="button" onClick={submitForm} className="btn btn-primary mt-4">Registrar</button>
                 </div>
             </div>
-        </div>
+        </form>    
     )
 }
 
